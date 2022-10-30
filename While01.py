caf9537d2a4c925f1a7305f4173d96275b2cdb16
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2022-10-30 04:23:57 -0700

    01

--- While01.py
+++ While01.py
@@ -9,8 +9,8 @@ def main(s):
     i = 0
     a = 0
     while i < len(s):
-        if s[i].isalpha():
+        if s[i].isdigit():
             a+=1
         i+=1
     return a
-print(main("Samarqand ff20221030"))
+print(main("Samarqand 20221030"))
